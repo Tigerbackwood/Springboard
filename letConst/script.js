a287989a0eb41f25ec015a4fde5fb7d59fbd8563
Devon Greenbaum <84977200+Tigerbackwood@users.noreply.github.com>
2021-06-28 08:34:57 -0700

    Add files via upload

---
+++ letConst/script.js
@@ -0,0 +1,9 @@
+const PI = 3.14;
+// Question 1
+// The difference between var and let is var can be redeclared and let cannot be redeclared. Var follows function scope let folllows block scope.
+//question 2
+//The difference between var and const is var can be redeclared and reassigned const cannot be redeclared. Var follows function scope Const follows block scope.
+//question 3
+//The difference between let and const is let can be reassigned and const cannot.
+//question 4
+//var is read first in its scope.
